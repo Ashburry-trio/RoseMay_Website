6f318c38cef4fcbd071b46398665d7e6aa816e15
ashburry-chat-irc <ashburry_undernet@outlook.com>
2024-05-16 16:57:49 +0000

    Modify the website to work as www.myproxyip.com and the naked domain of that.  I also set up an email server but that is not part of this webserver. Created a Privacy Policy v1.0pre.

--- www/website/views.py
+++ www/website/views.py
@@ -10,22 +10,46 @@ views = Blueprint('views', __name__, template_folder='templates', static_folder=
 def handle_rate_limit_exceeded(e):
     return jsonify({'tooManyRequests': 'the webserver is flooded with traffic. Try again, whenever you are ready...' }), 429
 
-@app.errorhanler(404)
-    def no_found(e):
-        gk.report()
-        return render_template('404.html'), 404
+@app.errorhandler(404)
+def no_found(e):
+    gk.report()
+    return render_template('404.html'), 404
 
 @views.route('/casino.html', methods=['GET', 'POST'])
-@gk.specific(rate_limit_rules=[{"count":1,"window":1,"duration":240}])
+@gk.specific(rate_limit_rules=[{"count":1,"window":100,"duration":2400}])
 def skinners_version_ignore():
     gk.report()
-    response = Response('You have been banned for visiting this page. You probably found this page in someones "/ctcp bauderr_ version" reply. That is a dangerous person who you should ignore.', status=429, mimetype='text/plain')
+    response = Response('You have been banned for visiting this page. You probably found this page in someones "/ctcp bauderr_ version" reply. If you received this webpage in a version reply from a user on IRC; that is a dangerous person whom you should ignore.', status=403, mimetype='text/plain')
     return response
 
-@views.route('/nick.txt/', methods=['GET', 'POST'])
-def asdfasdfasf():
-    return make_response(render_template('nick.txt'), 200)
-
+@views.route('/_asterisk/', methods=['GET', 'POST'])
+@views.route('/admin/assets/js/views/login.js', methods=['GET', 'POST'])
+@views.route('/admin/images/cal_date_over.gif', methods=['GET', 'POST'])
+@views.route('/ajax/', methods=['GET', 'POST'])
+@views.route('/api/v1/pods/', methods=['GET', 'POST'])
+@views.route('/bea_wls_deployment_internal/', methods=['GET', 'POST'])
+@views.route('/HNAP1//', methods=['GET', 'POST'])
+@views.route('/index.php/admin/', methods=['GET', 'POST'])
+@views.route('/jenkins/login/', methods=['GET', 'POST'])
+@views.route('/joomla/', methods=['GET', 'POST'])
+@views.route('/libs/js/iframe.js', methods=['GET', 'POST'])
+@views.route('/manager/html/', methods=['GET', 'POST'])
+@views.route('/muieblackcat/', methods=['GET', 'POST'])
+@views.route('/myadmin/', methods=['GET', 'POST'])
+@views.route('/mysqladmin/', methods=['GET', 'POST'])
+@views.route('/phpmyadmin/', methods=['GET', 'POST'])
+@views.route('/phpmyadmin1/', methods=['GET', 'POST'])
+@views.route('/phpmyadmin0/', methods=['GET', 'POST'])
+@views.route('/pma/', methods=['GET', 'POST'])
+@views.route('/remote/login/', methods=['GET', 'POST'])
+@views.route('/sftp-config.json', methods=['GET', 'POST'])
+@views.route('/solr/', methods=['GET', 'POST'])
+@views.route('/TP/public/index.php', methods=['GET', 'POST'])
+@views.route('/sql/', methods=['GET', 'POST'])
+@views.route('/temp/wp-admin/', methods=['GET', 'POST'])
+@views.route('/templates/system/css/system.css', methods=['GET', 'POST'])
+@views.route('/wp-content/plugins/image-clipboard/readme.txt', methods=['GET', 'POST'])
+@views.route('/api/jsonws/invoke/', methods=['GET', 'POST'])
 @views.route('/.well-known/', methods=['GET', 'POST'])
 @views.route('//wp-includes/wlwmanifest.xml', methods=['GET', 'POST'])
 @views.route('//xmlrpc.php', methods=['GET', 'POST'])
@@ -49,10 +73,16 @@ def asdfasdfasf():
 @views.route('/view-source:', methods=['GET', 'POST'])
 @views.route('/misc/ajax.js', methods=['GET', 'POST'])
 @views.route('/wp-login.php', methods=['GET', 'POST'])
-@gk.specific(rate_limit_rules=[{'count': 1, 'window': 60,"duration":240}])
+@views.route('/wizard/', methods=['GET', 'POST'])
+@views.route('/WebInterface/function/', methods=['GET', 'POST'])
+@views.route('/WebInterface/', methods=['GET', 'POST'])
+@views.route('/sabnzbd/', methods=['GET', 'POST'])
+@views.route('/sabnzbd/wizard/', methods=['GET', 'POST'])
+@gk.specific(rate_limit_rules=[{'count': 1, 'window': 60,"duration":2400}])
 def well_known_trap():
     gk.report()
-    return make_response(render_template('banned.txt'), 429)
+    return make_response(app.send_static_file('banned.txt'), 403)
+
 
 @views.route('/leakygut.html')
 def fixbiome():
@@ -64,6 +94,13 @@ def fixbiome():
 def index_home():
     gk.report()
     return render_template('index.html')
+
+
+@views.route('/policy.html', methods=['GET'])
+@views.route('/policy/', methods=['GET'])
+def policy_wwww():
+    gk.report()
+    return render_template('policy.html')
 
 
 @views.route('/hosted.html', methods=['GET'])
@@ -98,6 +135,13 @@ def contrib():
     gk.report()
     return render_template('contributing.html')
 
+@views.route('/privacypolicy/', methods=['GET'])
+@views.route('/policy/', methods=['GET'])
+@views.route('/privacypolicy.html', methods=['GET'])
+@views.route('/policy.html', methods=['GET'])
+def policy():
+    gk.report()
+    return render_template('policy.html')
 
 @views.route('/security.html', methods=['GET'])
 @views.route('/security/', methods=['GET'])
