3fdfa18fa761e8f190da92789eaec060bf019c7a
ashburry-chat-irc <ashburry_undernet@outlook.com>
2024-05-21 23:43:48 +0000

    Changed copfiles.sh small changes nothing to major

--- copyfiles.sh
+++ copyfiles.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Grant execute permission with: chmod +x copyfiles.sh
+# Execute with: ./copyfiles.sh
+# to copy symbolic links to another web-app directory.
 
-# Execute with: ./copyfiles.sh to copy
-# symbolic links to another web-app directory.
-
+mkdir ~/www_proxy
 ln -s ~/www/* ~/www_proxy
 
