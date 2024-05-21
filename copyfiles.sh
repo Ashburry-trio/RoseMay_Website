e946010972671755f86fd454671d9abb6c3f2f67
ashburry-chat-irc <ashburry_undernet@outlook.com>
2024-05-21 22:23:22 +0000

    Add images to the front page ALso created a copyfiles.sh which needs to be verified but it should link files to ~/www_proxy so I can have a second web-app

---
+++ copyfiles.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Grant execute permission with: chmod +x copyfiles.sh
+
+# Execute with: ./copyfiles.sh to copy
+# symbolic links to another web-app directory.
+
+ln -s ~/www/* ~/www_proxy
+
