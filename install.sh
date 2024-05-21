ed624fe815425dba15ec2dab98c3894e7509b6a7
ashburry-chat-irc <ashburry_undernet@outlook.com>
2024-05-21 23:49:29 +0000

    Added the instructions this time

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # Moves the files from the current directory
 # to the home directory where they will be usefull
-
+# First type: chmod +x ./install.sh
+# To run: ./install.sh
+`
+chmod +x ./copyfiles.sh
 mv * -t ~/ -f
 rm -f -r *
 cd ..
