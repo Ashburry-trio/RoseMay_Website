934d5cac75e36423462381d53d46902dd869af18
ashburry-chat-irc <ashburry_undernet@outlook.com>
2025-11-04 06:41:01 +0000

    Minor changes, no content added

---
+++ www/website/forms.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import Length, DataRequired
+
+class xSearchForm(FlaskForm):
+    search = StringField('XDCC Search',
+                         render_kw={"class": "form-control me-2", "placeholder": "XDCC Search", "aria-label": "Search for files"},
+                         validators=[DataRequired()])
+    search_button = SubmitField('Search', render_kw={"class": "btn btn-outline-success", "type": "submit"})
