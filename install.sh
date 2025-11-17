10442be3e9df54dc04f337632e6d84094b99d7a7
Kenggi "David" Peters <ashburryop@gmail.com>
2025-11-16 17:13:49 -0800

    Modify install script for improved usage
    Updated script to change how it is run and removed directory creation.

--- install.sh
+++ install.sh
@@ -2,9 +2,7 @@
 # Moves the files from the current directory
 # to the home directory where they will be usefull
 # First type: chmod +x ./install.sh
-# To run: ./install.sh
-mkdir RoseMay_Website
-cd RoseMay_Website
+# To run: source ./install.sh
 mv * -t ~/ -f
 cd ..
 rm -f -r ./RoseMay_Website
