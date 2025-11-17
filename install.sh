03b666f385308c3107d91ee0e38f985cb3c066de
Kenggi "David" Peters <ashburryop@gmail.com>
2025-11-16 17:55:08 -0800

    Revise install script and cleanup instructions
    Updated installation instructions and removed unnecessary directory creation.

--- install.sh
+++ install.sh
@@ -2,13 +2,7 @@
 # Moves the files from the current directory
 # to the home directory where they will be usefull
 # First type: chmod +x ./install.sh
-<<<<<<< HEAD
-# To run: ./install.sh
-mkdir RoseMay_Website
-cd RoseMay_Website
-=======
 # To run: source ./install.sh
->>>>>>> 70602f9b6dbcd29a9543baa862962fdf08d802c1
 mv * -t ~/ -f
 cd ..
 rm -f -r ./RoseMay_Website
